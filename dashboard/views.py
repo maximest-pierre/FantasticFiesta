be40cb89b69a89540e35c3ab30c05f866a1e2dc0
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2018-09-07 09:09:01 -0400

    Add dashboard view for event

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,6 +11,6 @@ class DashboardView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context['event'] = Event.objects.filter(user=self.request.user)
+        context['events'] = Event.objects.filter(user=self.request.user)
 
         return context
--- event/models.py
+++ event/models.py
@@ -15,7 +15,7 @@ class Event(models.Model):
     class Meta:
         verbose_name_plural = "Events"
 
-    contact = models.ManyToManyField(Contact, related_name='contact_event')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=STATUS_CHOICE)
     note = models.TextField()
     date = models.DateTimeField()
